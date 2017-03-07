67d5bc93743dd290f3c73f5f0743af66e02f2935
baymaxxx <81243232@qq.com>
2017-03-07 21:28:24 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <ul class="tab">
+      <li class="tab-item">
+        <router-link to="/foo">foo</router-link>
+      </li>
+      <li class="tab-item">
+        <router-link to="/bar">bar</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  .tab {
+    display: flex;
+    margin: 0 auto;
+    width: 1200px;
+    height: 40px;
+    line-height: 40px;
+    background: #137137;
+    .tab-item {
+      flex: 1;
+      border-right: 1px solid #fff;
+      text-align: center;
+      :last-child{
+        border-right: 0;
+      }
+      & > a {
+        color: #fff;
+      }
+    }
+  }
+</style>
